3a3c69e4d0dafe914eb19b57eaa0c9a847830918
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-10-02 15:48:39 +0600

    got to chat page

--- src/components/online/OnlineFriend.jsx
+++ src/components/online/OnlineFriend.jsx
@@ -3,6 +3,7 @@ import './onlineFriend.css'
 import { useEffect } from "react";
 import { getalluserinfo } from "../../actions/userinfo";
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 export default function OnlineFriend() {
   const dispatch = useDispatch();
   const alluserinfo = useSelector((state) => state.userinfo.alluserinfo);
@@ -22,15 +23,25 @@ export default function OnlineFriend() {
   //    ))}
   //  </div>
   <div>
-    {  Array.isArray(alluserinfo?.userInfor) && alluserinfo?.userInfor.map((userinfo) => (
-   <li className='rightbarFriend'>
-    <div className="rightbarProfileImageCont">
-        <img src={userinfo?.profileImg} alt="" className="rightbarProfileImage" />
-        <span className="rightbarOnline"></span>
+    
+      {Array.isArray(alluserinfo?.userInfor) && alluserinfo?.userInfor.map((userinfo) => (
+        <li className='rightbarFriend' key={userinfo._id}>
+          <div className="rightbarProfileImageCont">
+            {/* Wrap the image with a Link */}
+            {/* <Link to={`/chat/${userinfo._id}`}></Link> */}
+
+            
+            <Link to={"/chat"}>
+              <img src={userinfo?.profileImg} alt="" className="rightbarProfileImage" />
+            </Link>
+            <span className="rightbarOnline"></span>
+          </div>
+          {/* Wrap the name with a Link */}
+          <Link to={'/chat'} className="rightbarUsername">
+            <b >{userinfo?.name}</b>
+          </Link>
+        </li>
+      ))}
     </div>
-    <span className="rightbarUsername"><b>{userinfo?.name}</b></span>
-   </li>
-   ))}
-   </div>
   )
 }
