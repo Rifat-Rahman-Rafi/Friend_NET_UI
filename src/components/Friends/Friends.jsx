1ba8528ad0a4715990b660e48ba0bdbc856007e7
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-10-12 23:29:28 +0600

    friend list added

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { useEffect } from "react";
 import { getalluserinfo } from "../../actions/userinfo";
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia, Button, Grid, Container } from '@mui/material';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-
+import './Friends.css'
+import Topbar from '../topbar/Topbar';
+import Sidebar from '../sidebar/Sidebar';
 const Friends = () => {
 
    
 
     const dispatch = useDispatch();
   const alluserinfo = useSelector((state) => state?.userinfo?.alluserinfo);
-
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(getalluserinfo());
   }, [dispatch]);
+
+  function handle() {
+    navigate(`/profile/${alluserinfo?.creator}`);
+  }
+
+  console.log(alluserinfo.creator,"HELLOggggg")
+  
     return (
         <>
         {/* <div>
@@ -39,18 +48,29 @@ const Friends = () => {
             ))}
         </div> */}
 
+{/* 
+        <Container>
+
       <Typography variant="h4" align="center" gutterBottom>
         All Mutual Friends
       </Typography>
       <Grid container spacing={2}>
         {Array.isArray(alluserinfo?.userInfor) && alluserinfo?.userInfor.map((userinfo) => (
-          <Grid key={userinfo._id} item xs={12} md={3}>
-            <Card >
-            <CardMedia
-        sx={{ height: 340 }}
-        image={userinfo?.profileImg}
-       
-      />
+          <Grid key={userinfo._id} item xs={12} md={4}>
+            <Card>
+            <Container style={{ height: "350px" }}>
+  <CardMedia
+    style={{
+      width: "100%",
+      height: "100%",
+      objectFit: "cover", 
+      marginTop: "20px",
+    }}
+    image={userinfo?.profileImg}
+    title="green iguana"
+  />
+</Container>
+
               <CardContent>
                 <Typography variant="h5" component="div">
                   {userinfo?.name}
@@ -72,7 +92,44 @@ const Friends = () => {
         ))}
       </Grid>
 
-            
+      </Container> */}
+
+<Topbar></Topbar>
+<div style={{display:"flex",marginTop:"60px"}}>
+   <div> <Sidebar></Sidebar></div>
+<Container style={{justifyContent:"center"}}>
+    <h1 style={{textAlign:"center",marginTop:"30px",marginBottom:"50px"}}>ALL Mutual Friends</h1>
+  <div className="card-container">
+    {Array.isArray(alluserinfo?.userInfor) &&
+      alluserinfo?.userInfor.map((userinfo) => (
+        <div className="card" key={userinfo?._id}>
+          <img style={{width:"100%",height:"260px"}} src={userinfo?.profileImg} className="card-img-top" alt="Profile" />
+          <div className="card-body">
+          <p className="card-text">
+             Bio : {userinfo?.bio}
+            </p>
+            <h3 className="card-text">
+             Name : {userinfo?.name}
+            </h3>
+          </div>
+          <Button
+          style={{marginBottom:"50px"}}
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  onClick={handle}
+                >
+                  View Profile
+                </Button>
+        </div>
+      ))}
+  </div>
+  </Container>
+</div>
+
+
+
+
             </>
 
 
