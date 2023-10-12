eb60d1101185d856d937d03dd612385c7fd41a8d
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-10-13 00:20:46 +0600

    show mutual friend and follow Users

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect } from "react";
-import { getalluserinfo, getuserinfo } from "../../actions/userinfo";
+import { getalluserinfo, getfollowers, getuserinfo } from "../../actions/userinfo";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia, Button, Grid, Container } from '@mui/material';
@@ -12,7 +12,7 @@ const Friends = () => {
 
    
 
-    const dispatch = useDispatch();
+const dispatch = useDispatch();
   const alluserinfo = useSelector((state) => state?.userinfo?.alluserinfo);
   const navigate = useNavigate();
   useEffect(() => {
@@ -29,12 +29,38 @@ const Friends = () => {
     navigate(`/profile/${creatorId}`); 
   };
 
+  const user = JSON.parse(localStorage.getItem("profile"));
+  const { id } = useParams();
+
   let allIds = userinfo?.data?.userInfor?.followeeId ;
   let allIdes2 = userinfo?.data?.userInfor?.followerId;
   
   allIds = allIds?.concat(allIdes2);
 
-  console.log("SHSJS",allIds)
+  allIds= allIds?.concat(user?.result?._id);
+
+
+
+
+
+
+
+
+
+
+  const handleFollow = () => {
+    dispatch(
+
+      getfollowers({
+        follower: user?.result?._id,
+        followee: id,
+        userInfoid: userinfo?.data?.userInfor?._id,
+        follower_name: user?.result?.name,
+      })
+    );
+  };
+
+  console.log("SHSJS",allIds.length,alluserinfo?.userInfor?.length)
 
   
     return (
@@ -170,13 +196,16 @@ const Friends = () => {
       ))} */}
   </div>
 
+ {
+    allIds.length!==alluserinfo?.userInfor?.length?
+    <h1 style={{textAlign:"center",marginTop:"30px",marginBottom:"50px"}}>Follow Users</h1>:<></>
+ }
 
-  <h1 style={{textAlign:"center",marginTop:"30px",marginBottom:"50px"}}>ALL Mutual Friends</h1>
   <div className="card-container">
 
   {Array.isArray(alluserinfo?.userInfor) && allIds.length > 0 ? (
   alluserinfo?.userInfor
-    .filter((userinfo) => allIds.includes(userinfo.creator))
+    .filter((userinfo) =>!allIds.includes(userinfo.creator))
     .map((userinfo) => (
       <div className="card" key={userinfo?._id}>
         <img style={{ width: "100%", height: "260px" }} src={userinfo?.profileImg} className="card-img-top" alt="Profile" />
@@ -184,15 +213,11 @@ const Friends = () => {
           <p className="card-text">Bio: {userinfo?.bio}</p>
           <h3 className="card-text">Name: {userinfo?.name}</h3>
         </div>
-        <Button
-          style={{ marginBottom: "50px" }}
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={() => handleViewProfile(userinfo?.creator)}
-        >
-          View Profile
-        </Button>
+        <Button variant="contained"   type="button" onClick={handleFollow} style={{margin: "5px" ,color:"white",width: "75%"}}>
+            {userinfo?.data?.userInfor?.followeeId?.includes(user?.result?._id)
+              ? "Unfollow"
+              : "Follow"}
+          </Button>
       </div>
     ))
 ) : (
